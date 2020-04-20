effae4260ae499c85049cb9744755c9be311e8ad
Ksenia Kuntsevich <kse.kuntsevich@icloud.com>
2020-04-20 17:51:39 +0300

    feat: .env mongo connection

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-// const Board = require('../resources/boards/board.memory.repository').save;
-// const serSave = require('../resources/users/user.memory.repository').save;
+const { MONGO_CONNECTION_STRING } = require('../common/config');
 
 const boards = [
   {
@@ -20,21 +19,16 @@ const users = [
 ];
 
 const connectToDB = cb => {
-  mongoose.connect(
-    'mongodb+srv://admin:admin@cluster0-quoxp.mongodb.net/rest?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  );
+  mongoose.connect(MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', async () => {
     console.log("we're connected!");
     await db.dropDatabase();
-    // users.forEach(d => User.save(d));
-    // boards.forEach(d => Board.save(d));
     cb();
   });
 };
